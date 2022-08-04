4bc77829e889fea074efa776e6edc0469cd7c66c
Ira Iosub <ira.iosub@gmail.com>
2022-08-04 14:47:34 +0100

    added options

--- germs.R
+++ germs.R
@@ -1,36 +1,45 @@
+#!/usr/bin/env Rscript
 # library(germs)
+suppressPackageStartupMessages(library(Biostrings))
+suppressPackageStartupMessages(library(optparse))
 
-library(Biostrings)
-library(tidyverse)
 
+option_list <- list(make_option(c("-f", "--fasta"), action = "store", type = "character", help = "Input FASTA file with sequences)"),
+                    make_option(c("-k", "--k_length"), action = "store", type = "integer", help = "k-mer length [default: %default]", default = 5),
+                    make_option(c("-w", "--window_size"), action = "store", type = "integer", help = "Window size [default: %default]", default = 123),
+                    make_option(c("-s", "--smoothing_size"), action = "store", type = "integer", help = "Smoothing window size [default: %default]", default = 123),
+                    make_option(c("-o", "--output"), action = "store", type = "character", help = "Output filename"),
+                    make_option(c("", "--verbose"), action = "store_true", type = "logical", help = "Verbose", default = FALSE))
+
+opt_parser = OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
 
 # Parameters --------------------------------------------------------------
 
-k_len = 5
-window_size = 123
+if(opt$verbose) print(opt)
 
-# window_size must be odd!
-if(window_size %% 2 == 0){
-  window_size = window_size + 1
+if(opt$window_size %% 2 == 0) {
+
+  message("Incrementing window size by 1 to satisfy parity requirements.") # window_size must be odd!
+  opt$window_size = opt$window_size + 1
+
 }
 
 # Build scoring matrices -----------------------------------------
 
-hdm <- create_hamming_distance_matrix(k_len)
-pdv <- create_positional_distance_vector(window_size, k_len)
+hdm <- create_hamming_distance_matrix(opt$k_length)
+pdv <- create_positional_distance_vector(opt$window_size, opt$k_length)
 
 # Load sequences ----------------------------------------------------------
 
-sequences <- readDNAStringSet("/camp/lab/ulej/home/users/farawar/genomes/hs/fasta/longest_gencode29.fa") %>% as.character()
-
-transcript_details <- read_tsv("/camp/lab/ulej/home/users/farawar/GASR/lists/longest_proteincoding_transcript_hs_details.txt", col_types = cols()) %>% #Suppress messages with col_types=cols()
-  filter(cds_length > (window_size + k_len - 2)) #If the coding sequence is too short for the multivalency calculation, remove the entry.
-
-sequences <- sequences[match(transcript_details$transcript_id, names(sequences))] #Reorder sequences to match transcript detail table
+sequences <- as.character(readDNAStringSet(opt$fasta))
+sequences <- sequences[nchar(sequences) >= opt$window_size]
 
 # Calculate multivalency --------------------------------------------------
 
-all_kmer_multivalency <- list_kmer_multivalencies(sequences, k_len, window_size, hdm, pdv)
+all_kmer_multivalency <- list_kmer_multivalencies(sequences, opt$k_length, opt$window_size, hdm, pdv)
+all_local_multivalency <- list_sliding_means(all_kmer_multivalency, opt$smoothing_size)
 
-all_local_multivalency <- list_sliding_means(all_kmer_multivalency, window_size)
+# Re-format results --------------------------------------------------
+
 
