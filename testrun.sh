c5a196f0639fad78037166eb1213122f08d76f5f
Ira Iosub <ira.iosub@gmail.com>
2022-08-12 15:11:25 +0100

    generate automatic output name with suffix indicating window and smoothing size if output name is not specified by user

--- testrun.sh
+++ testrun.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Test run for standard sequences
-Rscript germs.R -f test_data/test.fasta -o output.tsv.gz -w 122 -s 122 -t transcripts.txt -p plots
+Rscript germs.R -f test_data/test.fasta -w 122 -s 122 -t transcripts.txt -p plots
 
-# Test run for sequences non-standard characters
-Rscript germs.R -f test_data/test_nonstdchars.fasta -o output_nonstdchars.tsv.gz -w 122 -s 122 -t transcripts_nonstdchars.txt -p plots_nonstdchars
+# # Test run for standard sequences
+# Rscript germs.R -f test_data/test.fasta -o output.tsv.gz -w 122 -s 122 -t transcripts.txt -p plots
+
+# # Test run for sequences non-standard characters
+# Rscript germs.R -f test_data/test_nonstdchars.fasta -o output_nonstdchars.tsv.gz -w 122 -s 122 -t transcripts_nonstdchars.txt -p plots_nonstdchars
 
