62703fc04623f0e7482a7fb007e1e18828323a67
Ira Iosub <ira.iosub@gmail.com>
2022-08-12 12:17:23 +0100

    refactored the nonstdchars code into the df output function, added test run

--- testrun.sh
+++ testrun.sh
@@ -4,4 +4,5 @@
 Rscript germs.R -f test_data/test.fasta -o output.tsv.gz -w 122 -s 122 -t transcripts.txt -p plots
 
 # Test run for sequences non-standard characters
-Rscript germs.R -f test_data/test_nonstdchars.fasta -o output_nonstdchars.tsv.gz -w 122 -s 122
+Rscript germs.R -f test_data/test_nonstdchars.fasta -o output_nonstdchars.tsv.gz -w 122 -s 122 -t transcripts_nonstdchars.txt -p plots_nonstdchars
+
